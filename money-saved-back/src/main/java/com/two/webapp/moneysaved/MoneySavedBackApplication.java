370131169c5556909ec7f1af921c2db0de77b63c
fondjof <fionaf>
2023-09-28 21:48:26 +0200

    initial commit back end

---
+++ money-saved-back/src/main/java/com/two/webapp/moneysaved/MoneySavedBackApplication.java
@@ -0,0 +1,13 @@
+package com.two.webapp.moneysaved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneySavedBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneySavedBackApplication.class, args);
+	}
+
+}
---
+++ money-saved-back/src/test/java/com/two/webapp/moneysaved/MoneySavedBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.two.webapp.moneysaved;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneySavedBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
