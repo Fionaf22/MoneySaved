081ee5e269e21ea9640ab0551083b7f030c1a88c
fondjof <fionaf>
2023-12-04 12:48:35 +0100

    modif mapper

--- money-saved-back/src/main/java/com/two/webapp/moneysaved/mapper/model_entity/DetailDepenseMapperME.java
+++ money-saved-back/src/main/java/com/two/webapp/moneysaved/mapper/model_entity/DetailDepenseMapperME.java
@@ -1,12 +1,24 @@
 package com.two.webapp.moneysaved.mapper.model_entity;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.two.webapp.moneysaved.entity.DetailDepenseEntity;
 import com.two.webapp.moneysaved.model.DetailDepense;
 
 public class DetailDepenseMapperME {
+	
+	@Autowired
+	DepenseMapperME depenseMapper;
+	
 	public DetailDepense entityToModel(DetailDepenseEntity entity) {
 		if (entity != null) {
-
+			DetailDepense model = new DetailDepense();
+			model.setArticle(entity.getArticle());
+			model.setDepense(depenseMapper.entityToModel(entity.getDepense()));
+			model.setId(entity.getId());
+			model.setMontant(entity.getMontant());
+			model.setUtile(entity.getUtile());
+			return model;
 		}
 		return null;
 
@@ -14,7 +26,13 @@ public class DetailDepenseMapperME {
 
 	public DetailDepenseEntity modelToEntity(DetailDepense model) {
 		if (model != null) {
-
+			DetailDepenseEntity entity = new DetailDepenseEntity();
+			entity.setArticle(model.getArticle());
+			entity.setDepense(depenseMapper.modelToEntity(model.getDepense()));
+			entity.setId(model.getId());
+			entity.setMontant(model.getMontant());
+			entity.setUtile(model.getUtile());
+			return entity;
 		}
 		return null;
 
